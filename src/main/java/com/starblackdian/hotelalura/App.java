4edf2ee5b3dd6c782070b095043018eb3271fc16
Diana <starblack_dian@hotmail.com>
2023-02-10 18:52:21 -0600

    Vistas en FXML
    Se crea la carpeta de Maven 'resources' yse agregan las vistas de la aplicación en FXML y los fondos de las mismas

--- src/main/java/com/starblackdian/hotelalura/App.java
+++ src/main/java/com/starblackdian/hotelalura/App.java
@@ -1,17 +1,8 @@
 package com.starblackdian.hotelalura;
 
-import com.starblackdian.hotelalura.model.dao.HuespedDao;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	try (HuespedDao dao = new HuespedDao()) {
-    		
-    	}
+    public static void main( String[] args ) {
+        
     }
 }
